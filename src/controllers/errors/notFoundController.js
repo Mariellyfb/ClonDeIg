5fe32a33d6257404ad11f44791f9452fdd5a3bbe
Mariellyfb <132379411+Mariellyfb@users.noreply.github.com>
2023-08-07 10:22:09 +0200

    Delete src directory

--- src/controllers/errors/notFoundController.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-const { notFoundError } = require('../../services/errorService');
-
-// Función controladora final que utilizaremos en el middleware de ruta no encontrada.
-const notFoundController = (req, res, next) => {
-    next(notFoundError('ruta'));
-};
-
-module.exports = notFoundController;
