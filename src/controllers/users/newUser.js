d8006d9ba7e330f94e1828a14b4a4fdd011342bc
HACKER <martuchi010903@gmail.com>
2023-08-29 01:59:14 +0200

    modificaciones post-entrega

--- src/controllers/users/newUser.js
+++ src/controllers/users/newUser.js
@@ -1,24 +1,52 @@
-const insertUSerModel = require("../../models/users/insertUserModel");
+/* const insertUSerModel = require('../../models/users/insertUserModel');
 
 const newUser = async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body;
+    try {
+        const { username, email, password } = req.body;
 
-    //Si falta algun campo lanzamos un error
-    if (!username || !email || !password) {
-      const err = new Error("Faltan campos");
-      err.httpStatus = 400;
-      throw err;
+        //Si falta algun campo lanzamos un error
+        if (!username || !email || !password) {
+            const err = new Error('Faltan campos');
+            err.httpStatus = 400;
+            throw err;
+        }
+        //Insertamos el usuario.
+        await insertUSerModel(username, email, password);
+
+        res.send({
+            status: 'ok',
+            message: 'Usuario creado',
+        });
+    } catch (err) {
+        next(err);
     }
-    //Insertamos el usuario.
-    await insertUSerModel(username, email, password);
-
-    res.send({
-      status: "ok",
-      message: "Usuario creado",
-    });
-  } catch (err) {
-    next(err);
-  }
 };
+module.exports = newUser; */
+
+const insertUSerModel = require('../../models/users/insertUserModel');
+
+const newUser = async (req, res, next) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // Si falta algún campo, lanzamos un error
+        if (!username || !email || !password) {
+            const err = new Error('Faltan campos');
+            err.httpStatus = 400;
+            throw err;
+        }
+
+        // Insertamos el usuario.
+        await insertUSerModel(username, email, password);
+
+        // Devolvemos un status 201 junto con la respuesta.
+        res.status(201).send({
+            status: 'created',
+            message: 'Usuario creado',
+        });
+    } catch (err) {
+        next(err);
+    }
+};
+
 module.exports = newUser;
