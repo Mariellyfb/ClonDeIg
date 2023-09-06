bfea1b1903fdbb6bafe0fb7d085e989c3e11dcd4
HACKER <martuchi010903@gmail.com>
2023-09-06 18:40:13 +0200

    Agregados nuevos archivos y modificaciones en controladores y modelos de posts y usuarios

--- src/controllers/posts/index.js
+++ src/controllers/posts/index.js
@@ -1,8 +1,8 @@
 const getPostController = require('./getPostController.js');
 const likePostController = require('./likePostController');
 const newPostController = require('./newPostController');
-const getPostsUserController = require("./getPostsUserController")
-const getPostsController = require("./getPostsController")
+const getPostsUserController = require('./getPostsUserController');
+const getPostsController = require('../posts/getPostController.js');
 
 module.exports = {
     getPostController,
